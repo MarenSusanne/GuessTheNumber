64673166fd30acbf70351b0044f9545c1ae5d0c2
MarenSusanne <Marensut@getacademy.no>
2024-05-16 15:48:10 +0200

    Add project files.

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿start();
+var correctNr = 0;
+void start()
+{
+    Console.WriteLine("Number is chosen");
+    correctNr = getRndNumber();
+    Console.WriteLine("Guess the number between 1-100:");
+    main();
+}
+
+
+void main()
+{
+
+var guess = Convert.ToInt32(Console.ReadLine());
+if (guess > correctNr)
+{
+    Console.WriteLine("Too high. Try again:");
+    main();
+}else if (guess < correctNr)
+{
+    Console.WriteLine("Too low. Try again:");
+    main();
+}
+else if(guess == correctNr)
+{
+    Console.WriteLine("You got it!:)");
+    Console.WriteLine("Play again? y=yes/n=no");
+    var response = Console.ReadLine();
+    if (response == "y")
+    {
+            start();
+    }else if (response == "n")
+    {
+        Environment.Exit(0);
+    }
+}
+}
+
+
+
+int getRndNumber()
+{
+var rnd = new Random();
+return rnd.Next(1,101);
+}
